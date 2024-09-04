16a1f8a0f5e05d03d52712634a932739a70df5a7
LeeGaHyun12 <89252433+LeeGaHyun12@users.noreply.github.com>
2024-09-04 20:25:21 +0900

    Update AlertController.java

--- src/main/java/com/example/ssenotification/controller/AlertController.java
+++ src/main/java/com/example/ssenotification/controller/AlertController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:8080", "https://api.icebuckwheat.kro.kr"}, allowCredentials = "true")
+// @CrossOrigin(origins = {"http://localhost:8080", "https://api.icebuckwheat.kro.kr"}, allowCredentials = "true")
 @RequestMapping("/api/notification")
 public class AlertController {
 
