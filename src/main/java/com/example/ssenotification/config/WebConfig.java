e650f16ee948a1bb3a0e859a6e42cf13869f6820
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2024-09-08 13:21:30 +0900

    Update WebConfig.java

--- src/main/java/com/example/ssenotification/config/WebConfig.java
+++ src/main/java/com/example/ssenotification/config/WebConfig.java
@@ -1,19 +1,19 @@
- package com.example.ssenotification.config;
+ // package com.example.ssenotification.config;
 
- import org.springframework.context.annotation.Bean;
- import org.springframework.context.annotation.Configuration;
- import org.springframework.web.servlet.config.annotation.CorsRegistry;
- import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ // import org.springframework.context.annotation.Bean;
+ // import org.springframework.context.annotation.Configuration;
+ // import org.springframework.web.servlet.config.annotation.CorsRegistry;
+ // import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
- @Configuration
- public class WebConfig implements WebMvcConfigurer {
+ // @Configuration
+ // public class WebConfig implements WebMvcConfigurer {
 
-     @Override
-     public void addCorsMappings(CorsRegistry registry) {
-         registry.addMapping("/**")
-                 .allowedOrigins("http://localhost:8080") // React 애플리케이션의 주소
-                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                 .allowedHeaders("*")
-                 .allowCredentials(true);
-     }
- }
+ //     @Override
+ //     public void addCorsMappings(CorsRegistry registry) {
+ //         registry.addMapping("/**")
+ //                 .allowedOrigins("http://localhost:8080") // React 애플리케이션의 주소
+ //                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+ //                 .allowedHeaders("*")
+ //                 .allowCredentials(true);
+ //     }
+ // }
